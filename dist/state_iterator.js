1819d19527a813a9692f3f8f7c46d8469f3957f3
Steffan Sluis <steffansluis@gmail.com>
2015-10-05 04:47:37 +0200

    Fix 'some' function

--- dist/state_iterator.js
+++ dist/state_iterator.js
@@ -19,6 +19,7 @@ export class StateIterator {
         };
         this.state = state;
         this.range = range;
+        this.current = range[0];
     }
 }
 (function (StateIterator) {
@@ -33,47 +34,49 @@ export class StateIterator {
     function every(state, predicate, range) {
         var iterator = new StateIterator(state, range);
         function loop() {
-            return iterator.next().then(key => key == null || iterator.get().then(value => predicate(value, key)).then(result => result ? loop() : false));
+            return iterator.next().then(key => key == null || iterator.get().then(value => predicate(value, key)).then(result => result ? loop().then(() => true) : false));
         }
         return loop();
     }
     StateIterator.every = every;
     function some(state, predicate, range) {
-        return every(state, (value, key) => Promise.resolve(predicate(value, key)).then(result => !result)).then(result => !result);
+        var found = false;
+        return every(state, (value, key) => Promise.resolve(predicate(value, key)).then(result => { found = !!result; return !found; }), range).then((res) => found);
     }
     StateIterator.some = some;
     function forEach(state, fn, range) {
-        return every(state, (value, key) => Promise.resolve(fn(value, key)).then(() => true)).then(() => { });
+        return every(state, (value, key) => Promise.resolve(fn(value, key)).then(() => true), range).then(() => { });
     }
     StateIterator.forEach = forEach;
     function reduce(state, fn, memo, range) {
-        return forEach(state, (value, key) => Promise.resolve(fn(memo, value, key)).then(value => { memo = value; })).then(() => memo);
+        return forEach(state, (value, key) => Promise.resolve(fn(memo, value, key)).then(value => { memo = value; }), range).then(() => memo);
     }
     StateIterator.reduce = reduce;
     function toArray(state, range) {
-        return reduce(state, (memo, value) => (memo.push(value), memo), []);
+        return reduce(state, (memo, value) => (memo.push(value), memo), [], range);
     }
     StateIterator.toArray = toArray;
     function toObject(state, range) {
-        return reduce(state, (memo, value, key) => (memo[key] = value, memo), Object.create(null));
+        return reduce(state, (memo, value, key) => (memo[key] = value, memo), Object.create(null), range);
     }
     StateIterator.toObject = toObject;
     function findKey(state, fn, range) {
         var key;
-        return some(state, (v, k) => Promise.resolve(fn(v, k)).then(res => res ? (!!(key = k) || true) : false)).then(found => found ? key : null);
+        return some(state, (v, k) => Promise.resolve(fn(v, k)).then(res => res ? (!!(key = k) || true) : false), range)
+            .then(found => found ? key : null);
     }
     StateIterator.findKey = findKey;
     function find(state, fn, range) {
-        return findKey(state, fn).then(state.get);
+        return findKey(state, fn, range).then(state.get);
     }
     StateIterator.find = find;
     function keyOf(state, value, range) {
-        return findKey(state, v => v === value);
+        return findKey(state, v => v === value, range);
     }
     StateIterator.keyOf = keyOf;
     function indexOf(state, value, range) {
         var index = -1;
-        return some(state, (v, k) => (index++, value == v)).then((found) => { if (found) {
+        return some(state, (v, k) => (index++, value == v), range).then((found) => { if (found) {
             return index;
         }
         else {
@@ -82,15 +85,15 @@ export class StateIterator {
     }
     StateIterator.indexOf = indexOf;
     function keyAt(state, index, range) {
-        return findKey(state, () => 0 === index--);
+        return findKey(state, () => 0 === index--, range);
     }
     StateIterator.keyAt = keyAt;
     function at(state, index, range) {
-        return keyAt(state, index).then(state.get);
+        return keyAt(state, index, range).then(state.get);
     }
     StateIterator.at = at;
     function contains(state, value, range) {
-        return some(state, v => v === value);
+        return some(state, v => v === value, range);
     }
     StateIterator.contains = contains;
 })(StateIterator || (StateIterator = {}));
--- src/state_iterator.ts
+++ src/state_iterator.ts
@@ -10,6 +10,7 @@ export class StateIterator<V> {
   constructor(state: State<V>, range: Range = [null, null]) {
     this.state = state;
     this.range = range;
+    this.current = range[0];
   }
 
   get = (): Promise<V> => {
@@ -45,60 +46,62 @@ export module StateIterator {
 
     function loop(): Promise<boolean> {
       return iterator.next().then(key => key == null || iterator.get().then(value =>
-        predicate(value, key)).then(result => result ? loop() : false));
+        predicate(value, key)).then(result => result ? loop().then(() => true) : false));
     }
 
     return loop();
   }
 
   export function some<V>(state: State<V>, predicate: (value: V, key?: Key) => boolean | Promise<boolean>, range?: Range): Promise<boolean> {
-    return every(state, (value: V, key: Key) => Promise.resolve(predicate(value, key)).then(result => !result)).then(result => !result);
+    var found = false;
+    return every(state, (value: V, key: Key) => Promise.resolve(predicate(value, key)).then(result => {found = !!result; return !found}), range).then((res) => found);
   }
 
   export function forEach<V>(state: State<V>, fn: (value: V, key?: Key) => void | Promise<void>, range?: Range): Promise<void> {
-    return every(state, (value: V, key: Key) => Promise.resolve(fn(value, key)).then(() => true)).then(() => {})
+    return every(state, (value: V, key: Key) => Promise.resolve(fn(value, key)).then(() => true), range).then(() => {})
   }
 
   export function reduce<V, W>(state: State<V>, fn: (memo: W, value: V, key?: Key) => W | Promise<W>, memo?: W, range?: Range): Promise<W> {
-    return forEach(state, (value: V, key: Key) => Promise.resolve(fn(memo, value, key)).then(value => { memo = value })).then(() => memo);
+    return forEach(state, (value: V, key: Key) => Promise.resolve(fn(memo, value, key)).then(value => { memo = value }), range).then(() => memo);
   }
 
   export function toArray<V>(state: State<V>, range?: Range): Promise<V[]> {
-    return reduce<V, V[]>(state, (memo: V[], value: V) => (memo.push(value), memo), []);
+    return reduce<V, V[]>(state, (memo: V[], value: V) => (memo.push(value), memo), [], range);
   }
 
   export function toObject<V>(state: State<V>, range?: Range): Promise<V[]> {
-    return reduce<V, V[]>(state, (memo: V[], value: V, key: Key) => (memo[key] = value, memo), Object.create(null));
+    return reduce<V, V[]>(state, (memo: V[], value: V, key: Key) => (memo[key] = value, memo), Object.create(null), range);
   }
 
   export function findKey<V>(state: State<V>, fn: (value: V, key?: Key) => boolean | Promise<boolean>, range?: Range): Promise<Key> {
     var key: Key;
-    return some(state, (v: V, k: Key) => Promise.resolve(fn(v, k)).then(res => res ? (!!(key = k) || true) : false)).then(found => found ? key : null);
+    return some(state, (v: V, k: Key) => Promise.resolve(fn(v, k)).then(res => res ? (!!(key = k) || true) : false), range)
+      .then(found => found ? key : null);
   }
 
   export function find<V>(state: State<V>, fn: (value: V, key?: Key) => boolean | Promise<boolean>, range?: Range): Promise<V> {
-    return findKey(state, fn).then(state.get);
+    return findKey(state, fn, range).then(state.get);
   }
 
   export function keyOf<V>(state: State<V>, value: V, range?: Range): Promise<Key> {
-    return findKey(state, v => v === value);
+    return findKey(state, v => v === value, range);
   }
 
   export function indexOf<V>(state: State<V>, value: V, range?: Range): Promise<number> {
     var index = -1;
-    return some(state, (v: V, k: Key) => (index++, value == v)).then((found) => {if (found) {return index} else {throw new Error()}});
+    return some(state, (v: V, k: Key) => (index++, value == v), range).then((found) => {if (found) {return index} else {throw new Error()}});
   }
 
   export function keyAt<V>(state: State<V>, index: number, range?: Range): Promise<Key> {
-    return findKey(state, () => 0 === index--);
+    return findKey(state, () => 0 === index--, range);
   }
 
   export function at<V>(state: State<V>, index: number, range?: Range): Promise<V> {
-    return keyAt(state, index).then(state.get);
+    return keyAt(state, index, range).then(state.get);
   }
 
   export function contains<V>(state: State<V>, value: V, range?: Range): Promise<boolean> {
-    return some(state, v => v === value);
+    return some(state, v => v === value, range);
   }
 }
 
